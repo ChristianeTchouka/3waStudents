f733fe39bc5d91f6ce00b7437679cb226da19e36
ChristianeTchouka <ingridechristiane@gmail.com>
2023-11-14 12:04:44 +0100

    first commit 3waStudents

---
+++ app.js
@@ -0,0 +1,9 @@
+import chalk from "chalk";
+
+const name1=chalk.red('christaine')
+const name2=chalk.blue("Edmond")
+const name3=chalk.green("Ingrid")
+const name4=chalk.white("Steve")
+
+export  {name1,name2,name3,name4}
+
---
+++ index.js
@@ -0,0 +1,3 @@
+import chalk from 'chalk'
+import {name1,name2,name3,name4} from "./app.js"
+console.log(name1,name2,name3,name4)
